20d609f501cf3474bfd7a1d380f9cca9a4e5220e
Qiyuan Liang <qiyuliang2@gmail.com>
2022-12-07 22:01:05 +0100

    add parity missing case

--- test/performance_test/performance_test.go
+++ test/performance_test/performance_test.go
@@ -23,9 +23,15 @@ var InfoMap = map[string]FileInfo{
 		metaCID:    "QmeZmAZ7TiFRT7kqoV4oynn1STKwo8sbiwRnLsM21eZMCc",
 		totalBlock: 81,
 	},
+	"25MB": {
+		fileCID:    "QmNkkcM5tFMqWxdrekyZoJnF5QxWKZnqYdJFBUj1jssRhb",
+		metaCID:    "QmcnV4N1umtzBRk5fC6e8TYkTKhgFkwqxN6LjPZvTworwZ",
+		totalBlock: 101,
+	},
 }
 
 type TestResult struct {
+	successCnt     int
 	recoverRate    float32
 	downloadParity uint
 }
@@ -92,6 +98,7 @@ var recovery = func(fileinfo FileInfo, missingData map[int]struct{}, missingPari
 	}
 	walker(fileinfo.fileCID)
 
+	result.successCnt = successCount
 	result.recoverRate = float32(successCount) / float32(fileinfo.totalBlock)
 
 	var downloadParity uint = 0
@@ -122,16 +129,71 @@ func Test_Only_Data_Loss(t *testing.T) {
 				indexes = indexes[:len(indexes)-1]
 			}
 			result := recovery(fileinfo, missedIndexes, nil)
-			t.Logf("Success Data Recovery Rate: %f", result.recoverRate)
-			t.Logf("Success Parity Overhead: %d", result.downloadParity)
+			t.Logf("Data Recovery Rate: %f", result.recoverRate)
+			t.Logf("Parity Overhead: %d", result.downloadParity)
+			t.Logf("Successfully Downloaded Block: %d", result.successCnt)
 		}
 	}
-	
+
 	// missNum: 1
 	// Success Data Recovery Rate: 1.000000
 	// Success Parity Overhead: 2
 	// missNum: 81 (All blocks are missing)
 	// Success Data Recovery Rate: 1.000000
 	// Success Parity Overhead: 81
-	t.Run("test", onlyData(81, InfoMap["20MB"]))
+	t.Run("test", onlyData(100, InfoMap["25MB"]))
+}
+
+func Test_Only_Parity_Loss(t *testing.T) {
+	//var allRates []float32
+	//var allOverhead []float32
+	var accuRate float32
+	var accuOverhead uint
+	var accuSuccessCnt int
+
+	onlyParity := func(missNum int, fileinfo FileInfo) func(*testing.T) {
+		return func(*testing.T) {
+			indexes := make([][]int, 3)
+			for i := range indexes {
+				indexes[i] = make([]int, fileinfo.totalBlock)
+			}
+			for i := range indexes {
+				for j := 0; j < fileinfo.totalBlock; j++ {
+					indexes[i][j] = j
+				}
+			}
+
+			/* All data block is missing */
+			missedDataIndexes := map[int]struct{}{}
+			for i := 0; i < fileinfo.totalBlock; i++ {
+				missedDataIndexes[i] = struct{}{}
+			}
+
+			/* Some parity block is missing */
+			missedParityIndexes := []map[int]struct{}{{}, {}, {}}
+			for i := 0; i < missNum; i++ {
+				rOuter := int(rand.Int63n(int64(3)))
+				for len(indexes[rOuter]) == 0 {
+					rOuter = int(rand.Int63n(int64(3)))
+				}
+				rInner := int(rand.Int63n(int64(len(indexes[rOuter]))))
+				missedParityIndexes[rOuter][indexes[rOuter][rInner]] = struct{}{}
+				indexes[rOuter][rInner], indexes[rOuter][len(indexes[rOuter])-1] =
+					indexes[rOuter][len(indexes[rOuter])-1], indexes[rOuter][rInner]
+				indexes[rOuter] = indexes[rOuter][:len(indexes[rOuter])-1]
+			}
+			result := recovery(fileinfo, missedDataIndexes, missedParityIndexes)
+			t.Logf("Data Recovery Rate: %f", result.recoverRate)
+			t.Logf("Parity Overhead: %d", result.downloadParity)
+			t.Logf("Successfully Downloaded Block: %d", result.successCnt)
+			accuRate += result.recoverRate
+			accuOverhead += result.downloadParity
+			accuSuccessCnt += result.successCnt
+		}
+	}
+
+	//for missingCnt := 0; missingCnt < 312; missingCnt++ {
+	//
+	//}
+	t.Run("test", onlyParity(100, InfoMap["25MB"]))
 }
